e8eb13a5e3f8cdd9c347f1eafbee037929087107
slouc <sinisalouc@gmail.com>
2015-03-31 20:27:36 +0200

    function passing

---
+++ src/main/scala/examples/FunctionPassing.scala
@@ -0,0 +1,26 @@
+package examples
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+class FunctionPassing(val query: String) {
+
+  def isMatch(s: String) = s.contains(query)
+
+  def getMatchesFunctionReference(rdd: RDD[String]): RDD[String] = {
+    // Problem: "isMatch" means "this.isMatch" so we pass all of "this"
+    rdd.filter(isMatch)
+  }
+  
+  def getMatchesFieldReference(rdd: RDD[String]): RDD[String] = {
+    // Problem: "query" means "this.query" so we pass all of "this"
+    rdd.filter(x => x.contains(query))
+  }
+  
+  def getMatchesNoReference(rdd: RDD[String]): RDD[String] = {
+    // Safe: extract just the field we need into a local variable
+    val query_ = this.query
+    rdd.filter(x => x.contains(query))
+  }
+}
